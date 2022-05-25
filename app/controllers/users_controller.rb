54178d06b849d6b90295566287416220dea04023
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-05-25 12:17:40 +0000

    Railsチュートリアル8章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render 'new'
-    end
+      @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
   end
 
    private
